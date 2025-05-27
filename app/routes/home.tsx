68bd5ab407be55e6e179e3347d8affc8fc0c7553
JayceNgo <60013030+JayceNgo@users.noreply.github.com>
2025-05-27 14:40:31 -0400

    Slideshow P1

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,28 @@
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
-    return [
-        { title: "Hiep Ngo Grocery" },
-        { name: "description", content: "Welcome to HiepNgo Grocery!" },
-    ];
+    return [{ title: "Hiep Ngo Grocery" }, { name: "description", content: "Welcome to HiepNgo Grocery!" }];
 }
 
 export default function Home() {
-    return <div>Home</div>;
+    return (
+        <div className="slideshow">
+            <div className="slideshow__inner">
+                <div className="slideshow__item">
+                    <img src="/img/slideshow/item-1.png" alt="" className="slideshow__img" />
+                </div>
+                <div className="slideshow__item">
+                    <img src="/img/slideshow/item-1.png" alt="" className="slideshow__img" />
+                </div>
+                <div className="slideshow__item">
+                    <img src="/img/slideshow/item-1.png" alt="" className="slideshow__img" />
+                </div>
+            </div>
+            <div className="slideshow__page">
+                <span className="slideshow__num">1</span>
+                <span className="slideshow__slider"></span>
+                <span className="slideshow__num">5</span>
+            </div>
+        </div>
+    );
 }
