ea3f99d2011e32b6a0d2679ccb10f8775f3809d2
JayceNgo <60013030+JayceNgo@users.noreply.github.com>
2025-06-08 16:36:20 -0400

    Image Preview

--- app/routes/product-detail.tsx
+++ app/routes/product-detail.tsx
@@ -3,13 +3,13 @@ import Footer from "~/layouts/footer";
 export default function ProductDetail() {
     return (
         <>
-            <header>
+            <header className="header">
                 <Header></Header>
             </header>
             <main className="product-page">
                 <div className="container">
+                    {/* <!-- Breadcrumbs --> */}
                     <div className="product-container">
-                        {/* <!-- Breadcrumbs --> */}
                         <ul className="breadcrumbs">
                             <li>
                                 <a href="#!" className="breadcrumbs__link">
@@ -36,9 +36,48 @@ export default function ProductDetail() {
                             </li>
                         </ul>
                     </div>
+
+                    {/* <!-- Product info --> */}
+                    <div className="product-container">
+                        <div className="row">
+                            <div className="col-5">
+                                <div className="prod-preview">
+                                    <div className="prod-preview__list">
+                                        <div className="prod-preview__item">
+                                            <img src="/img/product/item-1.png" alt="" className="prod-preview__img" />
+                                        </div>
+                                        <div className="prod-preview__item">
+                                            <img src="/img/product/item-2.png" alt="" className="prod-preview__img" />
+                                        </div>
+                                        <div className="prod-preview__item">
+                                            <img src="/img/product/item-3.png" alt="" className="prod-preview__img" />
+                                        </div>
+                                        <div className="prod-preview__item">
+                                            <img src="/img/product/item-4.png" alt="" className="prod-preview__img" />
+                                        </div>
+                                    </div>
+                                    <div className="prod-preview__thumbs">
+                                        <img
+                                            src="/img/product/item-1.png"
+                                            alt=""
+                                            className="prod-preview__thumb-img prod-preview__thumb-img--current"
+                                        />
+                                        <img src="/img/product/item-2.png" alt="" className="prod-preview__thumb-img" />
+                                        <img src="/img/product/item-3.png" alt="" className="prod-preview__thumb-img" />
+                                        <img src="/img/product/item-4.png" alt="" className="prod-preview__thumb-img" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-7">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam dicta placeat officiis
+                                ea, accusamus nisi ab similique quisquam alias, repellendus consectetur asperiores
+                                excepturi quas sed repellat perferendis sint modi distinctio.
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </main>
-            <footer>
+            <footer className="footer">
                 <Footer></Footer>
             </footer>
         </>
